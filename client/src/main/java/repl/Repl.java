defb079216c3186641ac458d18ad96c7e10547ef
acsnow99 <acsnow99@gmail.com>
2024-03-13 19:24:42 -0600

    Register command accepts correct input

--- client/src/main/java/repl/Repl.java
+++ client/src/main/java/repl/Repl.java
@@ -1,5 +1,6 @@
 package repl;
 
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -10,17 +11,29 @@ public class Repl {
     public void run() {
         var clientReader = new Scanner(System.in);
         var line = "";
+        String[] lineItems;
+        String lineFirst;
         while (true) {
             cliString = "[" + loggedInStatusString + "] >>> ";
             System.out.print(cliString);
             line = clientReader.nextLine();
-            if (Objects.equals(line, "q") || Objects.equals(line, "quit")) {
+            lineItems = line.split(" ");
+            lineFirst = lineItems[0];
+
+            if (Objects.equals(lineFirst, "q") || Objects.equals(lineFirst, "quit")) {
                 return;
-            } else if (Objects.equals(line, "help")) {
+            } else if (Objects.equals(lineFirst, "help")) {
                 System.out.println("register <USERNAME> <PASSWORD> <EMAIL> - to create an account");
                 System.out.println("login <USERNAME> <PASSWORD> - to log in with an existing account and play");
                 System.out.println("quit - exit the chess CLI");
                 System.out.println("help - list possible commands (you just called this one)");
+            } else if (Objects.equals(lineFirst, "register")) {
+                if (lineItems.length < 4) {
+                    System.out.println("Missing username, password, or email");
+                } else {
+                    // TO-DO: actually register the user :)
+                    System.out.println("User " + lineItems[1] + " registered. Don't forget your password!");
+                }
             } else {
                 System.out.println("Could not recognize command - try typing 'help' for a list of available commands.");
             }

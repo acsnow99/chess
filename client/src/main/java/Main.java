87e626b9c11497825d3193f5984173f50fcd2704
acsnow99 <acsnow99@gmail.com>
2024-03-18 21:41:00 -0600

    Can register from CLI

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,10 +1,15 @@
 import chess.*;
+import server.Server;
 import ui.Repl;
 
 public class Main {
+
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
-        new Repl().run();
+        var server = new Server();
+        int port = 8080;
+        port = server.run(port);
+        new Repl().run(port);
     }
 }
--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,26 +1,37 @@
 package ui;
 
+import exceptions.HttpResponseException;
+import model.User;
+import serverFacade.ServerFacadeGame;
+import serverFacade.ServerFacadeRegistration;
+import serverFacade.ServerFacadeSession;
+
 import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Repl {
-    private String loggedInStatusString = "LOGGED OUT";
+    private String serverURL = "http://127.0.0.1:";
     private boolean loggedIn = false;
-    private String cliString;
+    private ServerFacadeRegistration facadeRegistration;
+    private ServerFacadeSession facadeSession;
+    private ServerFacadeGame facadeGame;
 
-    public void run() {
+    public void run(int port) {
+        initializeFacade(port);
+
         var clientReader = new Scanner(System.in);
         var line = "";
         String[] lineItems;
         String lineFirst;
         while (true) {
+            String loggedInStatusString = "LOGGED OUT";
             if (loggedIn) {
                 loggedInStatusString = "LOGGED IN";
             } else {
                 loggedInStatusString = "LOGGED OUT";
             }
-            cliString = "[" + loggedInStatusString + "] >>> ";
+            String cliString = "[" + loggedInStatusString + "] >>> ";
             System.out.print(cliString);
             line = clientReader.nextLine();
             lineItems = line.split(" ");
@@ -30,16 +41,12 @@ public class Repl {
                 return;
             } else if (!loggedIn) {
                 if (Objects.equals(lineFirst, "help")) {
-                    System.out.println("register <USERNAME> <PASSWORD> <EMAIL> - to create an account");
-                    System.out.println("login <USERNAME> <PASSWORD> - to log in with an existing account and play");
-                    System.out.println("quit - exit the chess CLI");
-                    System.out.println("help - list possible commands (you just called this one)");
+                    printHelpLoggedOut();
                 } else if (Objects.equals(lineFirst, "register")) {
                     if (lineItems.length < 4) {
                         System.out.println("Missing username, password, or email");
                     } else {
-                        // TO-DO: actually register and login the user :)
-                        System.out.println("User " + lineItems[1] + " logged in. Don't forget your password!");
+                        registerUser(lineItems[1], lineItems[2], lineItems[3]);
                     }
                 } else if (Objects.equals(lineFirst, "login")) {
                     if (lineItems.length < 3) {
@@ -54,19 +61,14 @@ public class Repl {
                 }
             } else {
                 if (Objects.equals(lineFirst, "help")) {
-                    System.out.println("create <NAME> - create a game");
-                    System.out.println("list - see all game info");
-                    System.out.println("join <ID> [WHITE|BLACK|<empty>] - join a game (empty joins as observer)");
-                    System.out.println("observe <ID> - observe a game");
-                    System.out.println("logout - when you are finished playing");
-                    System.out.println("quit - exit the chess CLI");
-                    System.out.println("help - list possible commands (you just called this one)");
+                    printHelpLoggedIn();
                 } else if (Objects.equals(lineFirst, "create")) {
                     if (lineItems.length < 2) {
                         System.out.println("Missing game name");
                     } else {
                         // TO-DO: actually create game :)
                         var gameName = lineItems[1];
+
                         System.out.println("Created game named " + gameName);
                     }
                 } else if (Objects.equals(lineFirst, "list")) {
@@ -105,6 +107,39 @@ public class Repl {
                     System.out.println("Could not recognize command - try typing 'help' for a list of available commands.");
                 }
             }
+        }
+    }
+
+    private void initializeFacade(int port) {
+        facadeRegistration = new ServerFacadeRegistration(serverURL + port);
+        facadeSession = new ServerFacadeSession(serverURL + port);
+        facadeGame = new ServerFacadeGame(serverURL + port);
+    }
+
+    private void printHelpLoggedOut() {
+        System.out.println("register <USERNAME> <PASSWORD> <EMAIL> - to create an account");
+        System.out.println("login <USERNAME> <PASSWORD> - to log in with an existing account and play");
+        System.out.println("quit - exit the chess CLI");
+        System.out.println("help - list possible commands (you just called this one)");
+    }
+
+    private void printHelpLoggedIn() {
+        System.out.println("create <NAME> - create a game");
+        System.out.println("list - see all game info");
+        System.out.println("join <ID> [WHITE|BLACK|<empty>] - join a game (empty joins as observer)");
+        System.out.println("observe <ID> - observe a game");
+        System.out.println("logout - when you are finished playing");
+        System.out.println("quit - exit the chess CLI");
+        System.out.println("help - list possible commands (you just called this one)");
+    }
+
+    private void registerUser(String username, String password, String email) {
+        try {
+            facadeRegistration.register(new User(username, password, email));
+            loggedIn = true;
+            System.out.println("User " + username + " logged in. Don't forget your password!");
+        } catch (HttpResponseException exception) {
+            System.out.println("Could not register user because of a server error... try again later");
         }
     }
 }
